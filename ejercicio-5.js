72f9348047a27afc4644b472ad778375d0fea2e7
Alvaro Garcia <34427195+alvarogarcia1010@users.noreply.github.com>
2018-09-21 15:04:42 -0600

    Update ejercicio-5.js

--- ejercicio-5.js
+++ ejercicio-5.js
@@ -1,7 +1,7 @@
 function radioCircunferencia () {
     r = prompt('Ingrese el radio de su circunferencia: ')
     if(r <= 0) {
-        return 'ERROR:El radio debe ser mayor que cero.';
+        return -1;
     }
     return Math.PI*Math.pow(r, 2);
 }
